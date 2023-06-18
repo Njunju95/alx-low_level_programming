54e720ac3d8dc9592be44d919df80ce7cbec006f
Njunju95 <nhlaponomvula07@gmail.com>
2023-06-17 22:31:34 -0700

    5-printf.c

--- 0x01-variables_if_else_while/5-printf.c
+++ 0x01-variables_if_else_while/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.n>
 /**
  * main - A program that print a line using the printf function
+ *
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a place of art,\n")
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 		return (0);
 }
