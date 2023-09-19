f0898bddf7d671f5637134ff137c97581eced0a8
Njunju95 <nhlaponomvula07@gmail.com>
2023-09-19 08:17:50 -0700

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - functio thats it
+ * @head: the head of DLL
+ * @index: the index of desierd node
+ * Return: the searched node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int	ind;
+
+	ind = 0;
+	while (head)
+	{
+		if (ind == index)
+			return (head);
+		ind++;
+		head = head->next;
+	}
+	return (NULL);
+}
